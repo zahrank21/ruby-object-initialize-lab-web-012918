559e8c8696878a74f47a4fd4da2c273ea0e56daf
Zahran Kabir <zahrank@gmail.com>
2018-01-30 17:57:51 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+  def initialize(dog_name, breed="Mutt")
+    @name = dog_name
+    @breed = breed
+  end
+
+  # def initialize(dog_breed = "Mutt")
+  #   @breed = dog_breed
+  # end
+
+end
